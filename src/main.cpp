4357821f5b9c176ec1c4398b9535d2480d4c2ad2
silent1603 <tamgiangbui@gmail.com>
2024-08-19 23:33:07 +0700

    * day 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,35 @@
-#include "windows.h"
+#include <windows.h>
+#include <memoryapi.h>
+#include "utils_type.h"
+
+global_variable bool g_running = false;
+global_variable void* g_bitmapMemory;
+global_variable BITMAPINFO g_bitmapInfo;
+
+
+internal void Win32_ResizeDIBSection(int width,int height) // Device independent bitmap
+{
+    if(g_bitmapMemory)
+    {
+        VirtualFree(g_bitmapMemory,0,MEM_RELEASE);
+    }
+    g_bitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFO);
+    g_bitmapInfo.bmiHeader.biWidth = width;
+    g_bitmapInfo.bmiHeader.biHeight = height;
+    g_bitmapInfo.bmiHeader.biPlanes = 1;
+    g_bitmapInfo.bmiHeader.biBitCount = 32;
+    g_bitmapInfo.bmiHeader.biCompression = BI_RGB;
+    int bytesPerPixel = 4;
+    int bitmapMemorySize = (width * height) * bytesPerPixel;
+    g_bitmapMemory = VirtualAlloc(0,bitmapMemorySize,MEM_COMMIT,PAGE_READWRITE);
+}
+
+internal void Win32_UpdateWindow(HDC deviceContext,int x,int y,int width,int height)
+{
+    StretchDIBits(deviceContext,x,y,width,height,
+                                x,y,width,height,
+                               g_bitmapMemory,&g_bitmapInfo,DIB_RGB_COLORS,SRCCOPY);
+}
 
 LRESULT CALLBACK MainWindowCallBack(HWND window, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -7,7 +38,12 @@ LRESULT CALLBACK MainWindowCallBack(HWND window, UINT message, WPARAM wParam, LP
     {
     case WM_CREATE:
     {
-         OutputDebugStringA("WM_CREATE\n");
+        RECT clientRect;
+        GetClientRect(window,&clientRect);
+        int width = clientRect.right -clientRect.left;
+        int height =clientRect.bottom - clientRect.top;
+        Win32_ResizeDIBSection(width,height);
+        OutputDebugStringA("WM_CREATE\n");
     }
     break;
     case WM_SIZE:
@@ -17,12 +53,12 @@ LRESULT CALLBACK MainWindowCallBack(HWND window, UINT message, WPARAM wParam, LP
     break;
     case WM_DESTROY:
     {
-        OutputDebugStringA("WM_DESTROY\n");
+        g_running = false;
     }
     break;
     case WM_CLOSE:
     {
-        OutputDebugStringA("WM_CLOSE\n");
+        g_running =false;
     }
     break;
     case WM_ACTIVATEAPP:
@@ -38,7 +74,7 @@ LRESULT CALLBACK MainWindowCallBack(HWND window, UINT message, WPARAM wParam, LP
         int y = paint.rcPaint.top;
         int height = paint.rcPaint.bottom - paint.rcPaint.top;
         int width = paint.rcPaint.left - paint.rcPaint.right;
-        
+        Win32_UpdateWindow(deviceContext,x,y,width,height);
         PatBlt(deviceContext,x,y,width,height,WHITENESS);
         EndPaint(window,&paint);
     }
@@ -67,8 +103,9 @@ int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE PreInstance, LPSTR CommandLin
         HWND windowHandle = CreateWindowEx(0,windowClass.lpszClassName,"CGames",WS_OVERLAPPEDWINDOW | WS_VISIBLE,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,0,0,Instance,0);
         if(windowHandle)
         {
+            g_running =true;
             MSG message;
-            for(;;)
+            while(g_running)
             {
                 BOOL messageResult = GetMessage(&message,0,0,0);
                 if(messageResult > 0)
---
+++ src/utils_type.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_TYPE
+#define UTILS_TYPE
+
+#define internal  static 
+#define local_persist  static 
+#define global_variable  static 
+
+#endif
